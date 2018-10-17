bef28f06ec374f29abd34c25ad1d85a2fd164b6e
kkt-623 <s163404@gmail.com>
2018-10-17 17:35:36 +0900

    単語のindex付けをする関数term_Indexerの修正
    コード記載の形式のレビュー文を重複無しの　term, index　の辞書にして出力

--- predealingtools.py
+++ predealingtools.py
@@ -2,19 +2,21 @@
 
 
 # Step.1 単語のindex付け
-def termIndexer():
+#フォーマットは <星の数> _SEP_ <分かち書き済レビュー文>
+def term_Indexer():
     term_index = {}
-    index_num = 1
+    index_num = 0
     with open("data/rakuten_reviews_wakati_test.txt", 'r', encoding="utf-8") as file:
         for l in file.readlines():
             if(l is None): break
             ents = l.split(" __ SEP __ ") #ents[0] 星の数、 ents[1] レビュー文
             if(len(ents) < 2): continue
-            terms = ents[1] #レビュー文
-            terms = terms.split(" ")
+            terms = ents[1] #分かち書き済レビュー文
+
+            terms = terms.split(" ")    # termsは単語のlist
+            # term_indexのkeyに単語が登録されていなければ、valueにindexを振っていく
             for term in terms:
-                term_index[index_num] = term
-                index_num+=1
+                term_index.setdefault(term, index_num + 1)
 
     return term_index
 
@@ -24,5 +26,4 @@ def termIndexer():
 # 1 1:0.12 2:0.4 3:0.1 ...
 # 0 2:0.59 4:0.1 5:0.01 ...
 
-result = termIndexer()
-print(result)
+print(term_Indexer())

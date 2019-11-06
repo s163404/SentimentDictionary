5b1f776384e5bb5fa08b82a99de8043463287b66
kkt-623 <s163404@gmail.com>
2019-11-06 17:38:30 +0900

    トピックモデル～トピッククラスタリング実装
    tools.py - テキストの前処理
    topicmodel.py - トピックモデルLDA、トピッククラスタ実装

--- topicmodel.py
+++ topicmodel.py
@@ -3,14 +3,17 @@
 import csv
 import emoji
 import gensim
+import numpy as np
+import os
 import re
 import sys
 import tools
+from sklearn.cluster import KMeans
 from pprint import pprint
 from pymongo import MongoClient
-from gensim import corpora
+from gensim import corpora, models
 
-def lda_test():
+os.environ['MALLET_HOME'] = "C:\\Users\\KMLAB-02\\mallet-2.0.8"
 """
 テキストを単語のリストに変換
 入力: テキスト集合
@@ -34,17 +37,17 @@ def trans_texts(N:int):
     texts = []
     for doc in docs:
         if len(texts) == N: break  # 文書数
-        if doc["text"] is None: continue
-        texts.append(doc["text"])
+        if doc["text"] is not None:
+            texts.append(doc["text"])
 
     print("テキストを単語のリストで表現")
     for text in texts:
         words = []
         frequency = {}
-
-        # 生テキストの前処理
+        # 生テキストを前処理
         text = tools.pre_process(text)
-        # chasen単語分割
+
+        # 単語分割と品詞特定
         nodes = tools.mecab_tolist(text)
         for node in nodes:
             word = node[0]
@@ -63,21 +66,24 @@ def trans_texts(N:int):
             if freq >= 1: words.append(word)
         if words: processed_texts.append(words)
 
-    f = open("Data/Topicmodel/texts{0}.csv".format(N), 'w', encoding="utf-8")
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerows(processed_texts)
-    f.close()
+    with open("Data/Topicmodel/texts{0}.csv".format(N), 'w', encoding="utf-8") as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(processed_texts)
+        f.close()
+
     return processed_texts
 
 
+"""
+トピックモデルの実装
+"""
 def lda_test():
     print("テキスト集合(単語リスト)を呼び出し")
     texts = []
     with open("Data/Topicmodel/texts100.csv", 'r', encoding="utf-8") as f:
-        reader = csv.reader(f)
-        for row in reader: texts.append(row)
+        for row in csv.reader(f): texts.append(row)
 
-    print("コーパス作成～LDA学習")
+    print("テキストコーパス作成～LDA学習")
     N = 15  # トピック数
 
     # 辞書
@@ -95,10 +101,31 @@ def lda_test():
     print("トピック(変分ベイズ)")
     pprint(lda_bayes.show_topics(num_topics=N))
 
+    topics = np.empty((0, 2), int)
+    print("トピックをクラスタリング")
     for n in range(N):
-        print("トピック：{0}".format(str(n)))
-        pprint(lda_bayes.get_topic_terms(n, 10))
+        for t in lda_bayes.get_topic_terms(n, 10):
+            id = t[0]
+            topics = np.append(topics, np.array([[n, id]]), axis=0)    # [トピック番号, 単語id]
 
+    kmeans_model = KMeans(n_clusters=5).fit(topics)
+    labels = kmeans_model.labels_
+    for label, topic in zip(labels, topics):
+        id = topic[1]
+        word = [key for key, value in w_id_dict.items() if value == id]
+        print(label, topic, word)
+
+
+
+    # """
+    # LDA+Gibbs sampling
+    # """
+    # corpora.malletcorpus.MalletCorpus.serialize("./corpus.mallet", corpus)
+    # mallet_corpus = corpora.malletcorpus.MalletCorpus("./corpus.mallet")
+    # mallet_path = "C:/Users/KMLAB-02/mallet-2.0.8/bin/mallet"
+    # lda_gibbs = models.wrappers.LdaMallet(mallet_path, mallet_corpus, num_topics=5, id2word=dictionary)
+    # print("トピック(ギブスサンプリング)")
+    # pprint(lda_gibbs.show_topics())
 
 def main():
     lda_test()

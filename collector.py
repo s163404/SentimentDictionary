fc369a824c8132143b7168cb053c220806a5cdbc
kkt-623 <s163404@gmail.com>
2019-11-12 20:57:26 +0900

    ツイートクローラー仮置き

--- collector.py
+++ collector.py
@@ -5,8 +5,10 @@ tweepyはPremium APIに対応していないため、代替としてTwitterAPI�
 
 """
 
+import sys
 import time
 from TwitterAPI import TwitterAPI
+from TwitterAPI import TwitterPager
 from TwitterAPI import TwitterError
 import csv
 from pymongo import MongoClient
@@ -18,13 +20,13 @@ client = MongoClient('localhost', 27017)
 # DB呼び出し
 db = client.tweet_db
 # コレクション呼び出し
-collection = db.collection3
+collection = db.goprohero8
 
 
 """
 Twitter API
 """
-SEARCH_TERM = 'iPhone11 lang:ja'
+SEARCH_TERM = '\"GoPro Hero8\"lang:ja'
 PRODUCT = '30day'
 LABEL = 'dev'
 
@@ -33,36 +35,76 @@ api = TwitterAPI("CdJ5dMMwhxqxcSp9hAKSBmYBK",
              "1179291145458704384-BJxih9B41KvDZf2lUOFKsRiO7WPilW",
              "z4mBuRCxEOrbNp1Z8bH3iGXoweQ4wrmiaHNJviplwsBJ5")
 
-# csvFile = open('Data/Twitter/10008.csv', 'w', encoding="utf-8")
-# csvWriter = csv.writer(csvFile)
 
-next = ''
-results = []
-while True:
-    r = api.request('tweets/search/%s/:%s' % (PRODUCT, LABEL),
-                {'query':SEARCH_TERM,
-                'fromDate':'201909300000',
-                'toDate':'201910020000'
-                })
-    if r.status_code != 200:    # レスポンスが正常でないとき
-        time.sleep(60*15)
-        r = api.request('tweets/search/%s/:%s' % (PRODUCT, LABEL),
-                        {'query': SEARCH_TERM,
-                         'fromDate': '201909190000',
-                         'toDate': '201910020000'
-                         })
-        if r.status_code != 200: break
+def search1():
+    csvFile = open('Data/Twitter/goprohero8.csv', 'w', encoding="utf-8")
+    csvWriter = csv.writer(csvFile)
 
-    for item in r:
-        print(item['text'])
-        # csvWriter.writerow([item['text'] if 'text' in item else item, item['id_str'], item['created_at']])
-        # post = {"text": item['text']  if 'text' in item else item,
-        #         "id": item['id_str'],
-        #         "created_at": item['created_at']
-        # }
-        # column = collection.insert_one(post)
+    next = ''
+    results = []
+    while True:
+        r = api.request('search/tweets/%s/:%s' % (PRODUCT, LABEL),
+                        {
+                            'query':SEARCH_TERM,
+                            'fromDate':'201910120000'
+                        }
+                        )
+        if r.status_code != 200:    # レスポンスが正常でないとき
+            time.sleep(60*15)
+            r = api.request('tweets/search/%s/:%s' % (PRODUCT, LABEL),
+                            {'query': SEARCH_TERM,
+                             'fromDate':'201910120000'
+                             })
+            if r.status_code != 200: break
 
-    json = r.json()
-    if 'next' not in json:      # nextidがないとき
-        break
-    next = json['next']
+        for item in r:
+            print(item['text'])
+            csvWriter.writerow([item['text'] if 'text' in item else item, item['id_str'], item['created_at']])
+
+            post = {"text": item['text'] if 'text' in item else item,
+                    "id": item['id_str'],
+                    "created_at": item['created_at']
+                    }
+            column = collection.insert_one(post)
+
+        json = r.json()
+        if 'next' not in json:      # nextidがないとき
+            break
+        next = json['next']
+
+
+def search2():
+    count = 0
+    r = TwitterPager(api, 'tweets/search/%s/:%s' % (PRODUCT, LABEL),
+                        {
+                            'query':SEARCH_TERM,
+                            'fromDate':'201910120000'
+                        }
+                    )
+    for item in r.get_iterator():
+        if 'text' in item:
+            if 'text' not in item: continue
+            # print(item['text'])
+            # print(item["id_str"])
+            # print(item["created_at"])
+            # print("=" * 60)
+            post = {"text": item['text'],
+                    "id": item['id_str'],
+                    "created_at": item['created_at']
+                    }
+            collection.insert_one(post)
+            count += 1
+            if count == 10000: break
+        elif 'message' in item and item['code'] == 88:
+            print('SUSPEND, RATE LIMIT EXCEEDED: %s\n' % item['message'])
+            break
+
+
+def main():
+    # search1()
+    search2()
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit()
--- crawler_toDB.py
+++ crawler_toDB.py
@@ -21,7 +21,7 @@ CS_LIST = []
 AT_LIST = []
 AS_LIST = []
 
-with open("C:/Users/s1634/Twitter_Keys/TwitterAPIkeys.csv", 'r', encoding="utf-8") as f:
+with open("C:/Users/KMLAB-02/Twitter_Keys/TwitterAPIkeys.csv", 'r', encoding="utf-8") as f:
         reader = csv.reader(f)
         header = next(reader)
         for row in reader:
@@ -128,64 +128,6 @@ def get_tagwords(id:str):
         return tag_words
 
 """
-ワード検索 -> MongoDBへ
-"""
-def get_tweets_byquery(word:str, max_id):
-    texts = []  # 取得したテキストのリスト
-    api, key_id = create_API(0)     # 初期APIインスタンス
-    limit = 40000
-
-    # MongoDB呼び出し
-    client = MongoClient('localhost', 27017)
-    db = client.tweet_db
-    collection = db.anonymous
-
-    # レートリミット検証
-    api, key_id = proc_ratelimit(api, key_id)
-
-    # 1回目取得
-    if max_id: results = api.search(word, lang='ja', count=100, max_id=str(max_id), result_type="recent")
-    else: results = api.search(word, lang='ja', count=100, result_type="recent")
-
-    while True:
-        # 取得結果の確認　-> max_id更新 -> DB格納
-        if len(results) is 0:
-            print("results なし")
-            break
-
-        current_max_id = str(results[-1].id - 1)      # 一番古いstatus id -1
-        for result in results:
-            print(result)
-            if re.match(r'^RT @.*', result.text): continue  # 公式RTはcontinue
-            # text = text_former(result.text)     # テキスト成形
-            post = {"text": result.text,
-                    "id": result.id_str,
-                    "created_at": result.created_at,
-                    "entities": result.entities
-                    }
-            column = collection.insert_one(post)
-
-        if collection.count() >= limit: break
-        # if len(texts) >= get_limit: break
-
-        # レートリミット検証と対応処理
-        api, key_id = proc_ratelimit(api, key_id)
-
-        # 2回目以降の取得
-        results = api.search(word, lang='ja', count=100, max_id=current_max_id, result_type="recent")
-        if len(results) is 0:
-            print("results なし API再生成")
-            api, key_id = create_API(key_id+1)
-
-    """
-    取得終了 -> ファイルwrite
-    取得できたところまでwriteする
-    """
-    # with open("Data/Twitter/iPhone11/texts_iphone11_10000.txt", 'w', encoding="utf-8") as f:
-    #     f.write('\n'.join(texts))
-
-
-"""
 レートリミットの検証と対応　api:twitterapiインスタンス,method:チェックしたいメソッド名
 """
 def proc_ratelimit(api, key_id):
@@ -207,16 +149,80 @@ def proc_ratelimit(api, key_id):
 
     return api, key_id
 
+"""
+ワード検索 -> MongoDBへ
+"""
+def get_tweets_byquery(word:str, max_id):
+    texts = []  # 取得したテキストのリスト
+    api, key_id = create_API(0)     # 初期APIインスタンス
+    limit = 40
+
+    # MongoDB呼び出し
+    client = MongoClient('localhost', 27017)
+    db = client.tweet_db
+    collection = db.goprohero8
+
+    # レートリミット検証
+    api, key_id = proc_ratelimit(api, key_id)
+
+    # 1回目取得
+    if max_id:
+        results = api.search(word, lang='ja', count=100, max_id=str(max_id), result_type="recent")
+    else:
+        results = api.search(word, lang='ja', count=100, result_type="recent")
+
+    while True:
+        # 取得結果の確認　-> max_id更新 -> DB格納
+        if len(results) is 0:
+            print("results なし")
+            break
+
+        current_max_id = str(results[-1].id - 1)      # 一番古いstatus id -1
+        for result in results:
+            print(result)
+            if re.match(r'^RT @.*', result.text): continue  # 公式RTはcontinue
+            # text = text_former(result.text)     # テキスト成形
+            post = {"text": result.text,
+                    "id": result.id_str,
+                    "created_at": result.created_at,
+                    "entities": result.entities
+                    }
+            collection.insert_one(post)
+
+        if collection.count() >= limit: break
+        # if len(texts) >= get_limit: break
+
+        # レートリミット検証と対応処理
+        api, key_id = proc_ratelimit(api, key_id)
+
+        # 2回目以降の取得
+        results = api.search(word, lang='ja', count=100, max_id=current_max_id, result_type="recent")
+        if len(results) is 0:
+            print("results なし API再生成")
+            api, key_id = create_API(key_id+1)
+            results = api.search(word, lang='ja', count=100, max_id=current_max_id, result_type="recent")
+
+
+    """
+    取得終了 -> ファイルwrite
+    取得できたところまでwriteする
+    """
+    # with open("Data/Twitter/iPhone11/texts_iphone11_10000.txt", 'w', encoding="utf-8") as f:
+    #     f.write('\n'.join(texts))
+
+
+
+
 
 """
 メイン
 """
 def main():
-    # get_tweets_byquery("\"iPhone 11\"|\"iPhone11\"", 1178046026990702600)
+    get_tweets_byquery("\"GoPro HERO8\"|\"gopro hero8\"|\"goprohero8\"", None)
     # get_tweetobj("\"虫さん\"")
     # print(get_tagwords("1184477648501669888"))
     # get_datatolist("id")
-    get_tweetsbyids()
+    # get_tweetsbyids()
 
 
 
